db461c5e0a7732882deab000eb20a753e9336c2a
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2022-09-13 00:43:57 +0000

    Add files via upload

---
+++ binomials.py
@@ -0,0 +1,20 @@
+import math
+
+def factorial(number):
+    if number == 0 or number == 1:
+        return number
+    else:
+        return number * factorial(number-1)
+
+
+def bionomials(number_1, number_2=2):
+    sum = 1
+    for i in range(1, number_1):
+        sum += (factorial(number_1)*math.pow(number_2, i))/factorial(i)
+        return sum
+
+
+n = int(input("enter the n value "))
+x = int(input("enter the x value "))
+
+print(bionomials(n, x))
